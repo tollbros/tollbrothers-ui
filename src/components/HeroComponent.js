97081b32e57dee99b641a79d9f4de3d28c61d3fb
Sara Meixner <smeixner@tollbrothers.com>
2022-12-12 13:13:41 -0500

    chore: updating documentation to reflect new file structure

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Fragment } from "react";
-// import styles from '../styles.module.scss';
 import styles from './HeroComponent.module.scss';
 import Link from "next/dist/client/link";
 

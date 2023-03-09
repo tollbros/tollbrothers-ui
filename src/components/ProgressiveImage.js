f4af08c88a6d1d136d52e92a51f9d56cf71b3a38
Sara Meixner <smeixner@tollbrothers.com>
2023-03-09 14:30:21 -0500

    fix: default color displays while hero image loads

---
+++ src/components/ProgressiveImage.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import styles from './ProgressiveImage.module.scss';
+
+const ProgressiveImage = ({ placeholderSrc, src, alt, isCurrent, id }) => {
+    const [imgSrc, setImgSrc] = useState(placeholderSrc || src);
+  
+    useEffect(() => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+            setImgSrc(src);
+        };
+    }, [src]);
+  
+    return (
+      <img
+        src={imgSrc}
+        alt={alt || ""}
+        {
+            ...isCurrent ?
+                {className: styles.currentImage}
+            :
+                {className: styles.nextImage}
+        }
+        id={id || ""}
+      />
+    );
+  };
+  export default ProgressiveImage;

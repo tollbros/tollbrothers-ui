168082230b24553b7b04ebcd4e6fe4edd4cd4f08
Chenzorama <chenzorama@hotmail.com>
2023-12-12 17:14:49 -0500

    onInput and defaultValue

--- src/components/DragSlider.js
+++ src/components/DragSlider.js
@@ -3,33 +3,13 @@ import styles from './DragSlider.module.scss'
 
 export const DragSlider = ({ number, setNumber, step, minValue, maxValue }) => {
 
-    const handleDrag = (e) => {
-        const dragTrack = document.getElementById('track');
-        const trackWidth = dragTrack.offsetWidth;
-        const trackRect = dragTrack.getBoundingClientRect();
-        const mouseX = e.touches ? e.touches[0].clientX - trackRect.left : e.clientX - trackRect.left;
-        const newValue = Math.round((mouseX / trackWidth) * (maxValue - minValue)) + minValue;
-        const slideValue = Math.max(minValue, Math.min(maxValue, newValue));
-        const steppededValue = Math.round(slideValue / step) * step;
-        setNumber(steppededValue);
+    const onInputHandler = (event) => {
+        const dragValue = event.target.value;
+        setNumber(dragValue);
     }
 
-
     return (
-        <div className={styles.dragSliderWrapper}
-            onMouseDown={(e) => {
-                handleDrag(e);
-                window.addEventListener('mousemove', handleDrag);
-                window.addEventListener('mouseup', () => {
-                    window.removeEventListener('mousemove', handleDrag);
-                });
-
-                window.addEventListener('touchstart', handleDrag);
-                window.addEventListener('touchend', () => {
-                    window.removeEventListener('touchmove', handleDrag);
-                });
-            }}
-        >
+        <div className={styles.dragSliderWrapper} >
 
             <div className={styles.track} id="track">
                 <input
@@ -39,11 +19,10 @@ export const DragSlider = ({ number, setNumber, step, minValue, maxValue }) => {
                     min={minValue}
                     max={maxValue}
                     step={step}
-                    value={number}
+                    defaultValue={number}
+                    onInput={onInputHandler}
                 />                
-               
             </div>
-
 
         </div>
     )

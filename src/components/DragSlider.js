287a1d67109630cbee3568ec459531b1af19c4f4
Charles Niedringhaus <cniedringhaus@tollbrothersinc.com>
2023-12-13 15:04:54 -0500

    fix: removing mouse and touch events

--- src/components/DragSlider.js
+++ src/components/DragSlider.js
@@ -1,35 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './DragSlider.module.scss'
 
 export const DragSlider = ({ number, setNumber, step, minValue, maxValue }) => {
 
-    const handleDrag = (e) => {
-        const dragTrack = document.getElementById('track');
-        const trackWidth = dragTrack.offsetWidth;
-        const trackRect = dragTrack.getBoundingClientRect();
-        const mouseX = e.touches ? e.touches[0].clientX - trackRect.left : e.clientX - trackRect.left;
-        const newValue = Math.round((mouseX / trackWidth) * (maxValue - minValue)) + minValue;
-        const slideValue = Math.max(minValue, Math.min(maxValue, newValue));
-        const steppededValue = Math.round(slideValue / step) * step;
-        setNumber(steppededValue);
-    }
 
+const sliderChange = (e) => {
+    setNumber(e.target.value)
+}
 
     return (
-        <div className={styles.dragSliderWrapper}
-            onMouseDown={(e) => {
-                handleDrag(e);
-                window.addEventListener('mousemove', handleDrag);
-                window.addEventListener('mouseup', () => {
-                    window.removeEventListener('mousemove', handleDrag);
-                });
-
-                window.addEventListener('touchstart', handleDrag);
-                window.addEventListener('touchend', () => {
-                    window.removeEventListener('touchmove', handleDrag);
-                });
-            }}
-        >
+        <div className={styles.dragSliderWrapper}>
 
             <div className={styles.track} id="track">
                 <input
@@ -39,7 +19,8 @@ export const DragSlider = ({ number, setNumber, step, minValue, maxValue }) => {
                     min={minValue}
                     max={maxValue}
                     step={step}
-                    value={number}
+                    defaultValue={number}
+                    onInput={sliderChange}
                 />                
                
             </div>

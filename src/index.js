9e825baddaaeced0158e0a319bbde0574a7770a1
philramirez <pramirez@tollbrothers.com>
2024-03-13 09:37:07 -0400

    feat: add Slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import Slider from './components/Slider'
+
 export { HeroComponent } from './components/HeroComponent.js'
 export { FullScreenGallery } from './components/FullScreenGallery.js'
 export { LoadingIndicator } from './components/LoadingIndicator'
 export { TollButton } from './components/TollButton'
 export { DragSlider } from './components/DragSlider'
 export { MortgageCalculator } from './components/MortgageCalculator'
-
-
+export { Slider }

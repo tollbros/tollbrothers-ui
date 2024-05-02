0a0b8d0c861fd84e04af19f482dd6f0f25ea927a
Charles Niedringhaus <cniedringhaus@tollbrothersinc.com>
2024-05-02 10:07:13 -0400

    fix: finishing up

--- src/components/HorizontalScroller.js
+++ src/components/HorizontalScroller.js
@@ -1,78 +1,57 @@
-import React, { useEffect } from 'react'
-import { useMemo, useState, useRef } from 'react';
+import React from 'react'
+import { useState, useRef } from 'react';
 import styles from './HorizontalScroller.module.scss'
 
 export const HorizontalScroller = ({ children, showArrows }) => {
+  const [isNextDisabled, setIsNextDisabled] = useState(false);
+  const [isPrevDisabled, setIsPrevDisabled] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const galleryRef = useRef(null);
+  const slideRef = useRef(null);
+  
+  const handleScroll = () => {
+    const gallery = galleryRef.current;
+    const newPosition = gallery.scrollLeft;
+    setScrollPosition(newPosition);
+    scrollPosition > (gallery.scrollWidth - gallery.clientWidth - 150) ? setIsNextDisabled(true) : setIsNextDisabled(false);
+    scrollPosition < 100 ? setIsPrevDisabled(true) : setIsPrevDisabled(false);
+  }
+
+  const handlePrev = () => {
+    const gallery = galleryRef.current;
+    const slide = slideRef.current;
+    gallery.scrollBy({ left: -(slide.offsetWidth + 20) }); // move gallery width of slide plus the margin for safari
+  };
+
+  const handleNext = () => {
+    const gallery = galleryRef.current;
+    const slide = slideRef.current;
+    gallery.scrollBy({ left: (slide.offsetWidth + 20) }); // move gallery width of slide plus the margin for safari
+  };
 
 
-    const [slideIndex, setSlideIndex] = useState(0);
-    const [isPrevDisabled, setIsPrevDisabled] = useState(false);
-    const [isNextDisabled, setIsNextDisabled] = useState(false);
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const galleryRef = useRef(null);
-    const slideCount = children.length;
+  return (
+    <div className={styles.horizontalScrollWrap}>
 
-
-    const handleScroll = () => {
-        if (galleryRef.current) {
-            setScrollPosition(galleryRef.current.scrollLeft);
-        }
-    };
-
-    const handlePrev = () => {
-        setSlideIndex((prevIndex) => prevIndex - 1)
-    };
-
-    const handleNext = () => {
-        setSlideIndex((prevIndex) => prevIndex + 1)
-        setIsPrevDisabled(false)
-    };
-    useEffect(() => {
-        scrollPosition === 0 ? setIsPrevDisabled(true) : setIsPrevDisabled(false)
-    }, [scrollPosition]);
-
-    useEffect(() => {
-        const scrollWrap = document.querySelector('.scrollWrap');
-        scrollWrap.addEventListener('scroll', (e) => {
-            setScrollPosition(galleryRef.current.scrollLeft);
-        });
-    });
-    
-    const transform = useMemo(() => {
-        if (!slideIndex) return 3
-        let i = 0
-        let newValue = 0
-        while (i < slideIndex) {
-            newValue = newValue - 28
-            i = i + 1
-
-        }
-        return newValue
-    }, [slideIndex])
-
-    
-
-    return (
-        <div className={styles.horizontalScrollWrap}>
-            {showArrows && (
-                <div className={styles.controls}>
-                    <button onClick={handlePrev} disabled={isPrevDisabled}><img src="https://cdn.tollbrothers.com/sites/comtollbrotherswww/svg/arrowexit.svg" alt="Arrow icon" /></button>
-                    <button onClick={handleNext} disabled={slideIndex === (slideCount - 1)}><img src="https://cdn.tollbrothers.com/sites/comtollbrotherswww/svg/arrowexit.svg" alt="Arrow icon" /></button>
-                </div>
-            )}
-            <div className={styles.viewPort}>
-
-                <div className={`${styles.scrollWrap} scrollWrap`} ref={galleryRef}>
-
-                    {Object.keys(children).map(key => (
-                        <>
-                            {/* <div className={styles.scrollItem} style={{ transform: `translate3d(${transform}px, 0px, 0px)` }}>{children[key]}</div> */}
-                            <div className={styles.scrollItem} style={{ transform: `translate3d(${transform}vw, 0px, 0px)` }}>{children[key]}</div>
-                        </>
-                    ))}
-
-                </div>
-            </div>
+      {showArrows && (
+        <div className={styles.controls}>
+          <button onClick={handlePrev} disabled={isPrevDisabled}><img src="https://cdn.tollbrothers.com/sites/comtollbrotherswww/svg/arrowexit.svg" alt="Arrow icon" /></button>
+          <button onClick={handleNext} disabled={isNextDisabled}><img src="https://cdn.tollbrothers.com/sites/comtollbrotherswww/svg/arrowexit.svg" alt="Arrow icon" /></button>
         </div>
-    )
+      )}
+
+      <div className={`${styles.viewPort} viewPort`}>
+
+        <div className={`${styles.scrollWrap} scrollWrap`} ref={galleryRef} onScroll={handleScroll}>
+
+          {Object.keys(children).map(key => (
+
+            <div className={styles.scrollItem} ref={slideRef} key={[key]}>{children[key]}</div>
+
+          ))}
+
+        </div>
+      </div>
+    </div>
+  )
 }

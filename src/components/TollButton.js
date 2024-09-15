53414a901d68c0b7fdb118576ed6d779e778aa93
Chenzorama <chenzorama@hotmail.com>
2024-09-15 12:35:25 -0400

    fix: tweak for update

--- src/components/TollButton.js
+++ src/components/TollButton.js
@@ -1,13 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styles from './TollButton.module.scss'
 
-export const TollButton = ({ children }) => {
-  const [animate, setAnimate] = useState(false)
-
-
-  return (
-    <div className={styles.tollButton}>
-        Button
-    </div>
-  )
+export const TollButton = () => {
+  return <div className={styles.tollButton}>Button</div>
 }

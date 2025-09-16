baa4329f6d70d6314c62280ddaf61c3970dcdb57
Michael Duarte <mduarte@tollbrothers.com>
2025-09-16 14:20:11 -0400

    fix: refactor chat

--- src/components/TollChat/TollChat.js
+++ src/components/TollChat/TollChat.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { ChatOsc } from './ChatOsc'
 import { fetchAvailability } from '../../../utils/chat/apis'

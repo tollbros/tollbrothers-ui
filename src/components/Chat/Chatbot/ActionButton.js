fbe3a2d4e50e7bd755fddcd7d42127dc3c2f48e2
Michael Duarte <mduarte@tollbrothers.com>
2026-02-02 13:52:14 -0500

    AWSCHAT-88 add link component to make all product names linkable

--- src/components/Chat/Chatbot/ActionButton.js
+++ src/components/Chat/Chatbot/ActionButton.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import styles from './ActionButton.module.scss'
 
-export const ActionButton = ({ children, onClick = () => null }) => {
+export const ActionButton = ({
+  children,
+  onClick = () => null,
+  showIcon = false
+}) => {
   return (
     <button className={styles.actionButton} onClick={onClick} type='button'>
       <span className={styles.text}>{children}</span>
-      <div className={styles.iconContainer}>
-        <img
-          src='https://cdn.tollbrothers.com/sites/comtollbrotherswww/icons/up-arrow.svg'
-          alt=''
-          className={styles.icon}
-        />
-      </div>
+      {showIcon && (
+        <div className={styles.iconContainer}>
+          <img
+            src='https://cdn.tollbrothers.com/sites/comtollbrotherswww/icons/up-arrow.svg'
+            alt=''
+            className={styles.icon}
+          />
+        </div>
+      )}
     </button>
   )
 }

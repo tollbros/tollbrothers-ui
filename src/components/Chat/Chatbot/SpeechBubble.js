c935a787cbf671ed08681fbe2ede6e98d927efc2
Charles Niedringhaus <cniedringhaus@tollbrothers.com>
2026-05-11 14:08:53 -0400

    fix: AWSCHAT-249, making intro hi speechbubble launch app like launch button

--- src/components/Chat/Chatbot/SpeechBubble.js
+++ src/components/Chat/Chatbot/SpeechBubble.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import styles from './SpeechBubble.module.scss'
 
-export const SpeechBubble = ({ isHidden = false }) => {
-  return (
-    <div className={`${styles.root} ${isHidden ? styles.hide : ''}`}>
-      <p className={styles.title}>Hello, I'm AI Concierge.</p>
-      <p className={styles.message}>Ask me about homes, communities, or anything you need.</p>
-      <div className={styles.arrow} />
-    </div>
-  )
-}
+export const SpeechBubble = ({ isHidden = false, onClick }) => (
+  <button className={`${styles.root} ${isHidden ? styles.hide : ''}`} onClick={onClick} type='button'>
+    <p className={styles.title}>Hello, I'm AI Concierge.</p>
+    <p className={styles.message}>Ask me about homes, communities, or anything you need.</p>
+    <div className={styles.arrow} />
+  </button>
+)
